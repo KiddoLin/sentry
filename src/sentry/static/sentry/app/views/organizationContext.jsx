ac681ebb7763734fd794dd7d0c7378f2c83a565b
davidw1339 <davidw1339@gmail.com>
2019-09-20 14:19:05 -0700

    Decomposing organizational details into lightweight org and two HOCs withUserTeams and withTeamProjects

--- src/sentry/static/sentry/app/views/organizationContext.jsx
+++ src/sentry/static/sentry/app/views/organizationContext.jsx
@@ -40,6 +40,7 @@ const OrganizationContext = createReactClass({
     useLastOrganization: PropTypes.bool,
     organizationsLoading: PropTypes.bool,
     organizations: PropTypes.arrayOf(SentryTypes.Organization),
+    lightweight: PropTypes.bool,
   },
 
   childContextTypes: {
@@ -116,13 +117,15 @@ const OrganizationContext = createReactClass({
   },
 
   fetchData() {
+    const {lightweight} = this.props;
     if (!this.getOrganizationSlug()) {
       this.setState({loading: this.props.organizationsLoading});
       return;
     }
-
     const promises = [
-      this.props.api.requestPromise(this.getOrganizationDetailsEndpoint()),
+      this.props.api.requestPromise(this.getOrganizationDetailsEndpoint(), {
+        query: {detailed: !!lightweight ? 0 : 1},
+      }),
       fetchOrganizationEnvironments(this.props.api, this.getOrganizationSlug()),
     ];
 
@@ -133,7 +136,6 @@ const OrganizationContext = createReactClass({
         HookStore.get('organization:header').forEach(cb => {
           hooks.push(cb(data));
         });
-
         setActiveOrganization(data);
 
         // Configure scope to have organization tag
@@ -141,8 +143,10 @@ const OrganizationContext = createReactClass({
           scope.setTag('organization', data.id);
         });
 
-        TeamStore.loadInitialData(data.teams);
-        ProjectsStore.loadInitialData(data.projects);
+        if (!lightweight) {
+          TeamStore.loadInitialData(data.teams);
+          ProjectsStore.loadInitialData(data.projects);
+        }
 
         // Make an exception for issue details in the case where it is accessed directly (e.g. from email)
         // We do not want to load the user's last used env/project in this case, otherwise will
@@ -152,7 +156,9 @@ const OrganizationContext = createReactClass({
             ({path}) => path && path.includes('/organizations/:orgId/issues/:groupId/')
           )
         ) {
-          GlobalSelectionStore.loadInitialData(data, this.props.location.query);
+          if (!lightweight) {
+            GlobalSelectionStore.loadInitialData(data, this.props.location.query);
+          }
         }
         OrganizationEnvironmentsStore.loadInitialData(environments);
 
